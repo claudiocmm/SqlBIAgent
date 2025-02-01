1a7af1aa0182ac6c06a00a68adfe21acb048ce9a
claudiocmm <claudiofilho22@gmail.com>
2025-02-01 11:07:38 -0300

    finishing requirements and setting readme

--- utils.py
+++ utils.py
@@ -5,6 +5,6 @@ def extract_code_block(content: str, language: str) -> str:
         match = re.search(rf'```{language}(.*?)```', content, re.DOTALL)
         extracted_code = match.group(1)
     except Exception:
-        extracted_code = content.replace(f"```{language}", "")
+        extracted_code = content.replace(f"```", "")
 
     return extracted_code
